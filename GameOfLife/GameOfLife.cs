6be176d1aff43d6eeb5b1b7af3a8f137aec4f86d
Ostap-Tadey Demchyshyn <ostap-tadey_demchyshyn@epam.com>
2024-06-22 10:35:27 +0300

    tests: add unit tests for GameOfLife class and create class skeleton

---
+++ GameOfLife/GameOfLife.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace GameOfLifeKata
+{
+    public class GameOfLife
+    {
+        private readonly int _width;
+        private readonly int _height;
+        private int[,] _currentGeneration;
+        public GameOfLife(int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetCellState(int row, int col, int value)
+        {
+            throw new NotImplementedException();
+        }
+        public int GetCellState(int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Show()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitCells()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NextGeneration()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GameOfLifeTests/GameOfLifeTests.cs
@@ -0,0 +1,105 @@
+using GameOfLifeKata;
+
+namespace GameOfLifeTests
+{
+    public class GameOfLifeTests
+    {
+        [Fact]
+        public void InitCells_ShouldSetAllCellsToZero()
+        {
+            var game = new GameOfLife(5, 5);
+
+            var cellsSum = SumAllCells(game);
+            Assert.Equal(0, cellsSum);
+        }
+
+        [Fact]
+        public void SetCellState_ShouldChangeCellState()
+        {
+            var game = new GameOfLife(5, 5);
+            game.SetCellState(2, 2, 1);
+
+            Assert.Equal(1, game.GetCellState(2, 2));
+        }
+
+        [Fact]
+        public void NextGeneration_ShouldKillSolitaryCell()
+        {
+            var game = new GameOfLife(5, 5);
+            game.SetCellState(2, 2, 1);
+
+            game.NextGeneration();
+
+            Assert.Equal(0, game.GetCellState(2, 2));
+        }
+
+        [Fact]
+        public void NextGeneration_ShouldKillCellWithMoreThanThreeNeighbours()
+        {
+            var game = new GameOfLife(5, 5);
+            game.SetCellState(2, 2, 1);
+            game.SetCellState(2, 3, 1);
+            game.SetCellState(3, 3, 1);
+            game.SetCellState(2, 4, 1);
+
+            game.NextGeneration();
+
+            Assert.Equal(0, game.GetCellState(2, 3));
+        }
+
+
+        [Fact]
+        public void NextGeneration_ShouldKillCellWithLessThanTwoNeighbours()
+        {
+            var game = new GameOfLife(5, 5);
+            game.SetCellState(2, 2, 1);
+            game.SetCellState(2, 3, 1);
+
+            game.NextGeneration();
+
+            Assert.Equal(0, game.GetCellState(2, 2));
+        }
+
+        [Fact]
+        public void NextGeneration_ShouldBringCellWithThreeNeighboursToLife()
+        {
+            var game = new GameOfLife(5, 5);
+            game.SetCellState(2, 2, 1);
+            game.SetCellState(3, 3, 1);
+            game.SetCellState(2, 4, 1);
+
+            game.NextGeneration();
+
+            Assert.Equal(1, game.GetCellState(2, 3));
+        }
+
+    
+
+        [Fact]
+        public void NextGeneration_ShouldKeepCellWithTwoNeighboursAlive()
+        {
+            var game = new GameOfLife(5, 5);
+            game.SetCellState(2, 2, 1);
+            game.SetCellState(2, 3, 1);
+            game.SetCellState(2, 4, 1);
+
+            game.NextGeneration();
+
+            Assert.Equal(1, game.GetCellState(2, 3));
+        }
+
+        // Helper method.
+        private int SumAllCells(GameOfLife game)
+        {
+            var sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    sum += game.GetCellState(i, j);
+                }
+            }
+            return sum;
+        }
+    }
+}
